c45eeffe54df7840bf559723658d9cb65921d924
John <160535164+Bugadu2@users.noreply.github.com>
2024-08-22 16:15:19 -0300

    Curriculo

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('sobre').textContent = "Gosto muito de programar mas ainda não sei se vou levar isso para minha vida como profissão,no meu tempo gosto de jogar vôlei e jogar video games tanto no celular como no meu playstation 3.";
+    document.getElementById('escolaridade').textContent = "Atualmente estou estudando no colégio Senac mas minha antiga escola se chamava Escola Municipal Pastor Hans Muller onde passei meu ensino fundamental inteiro.";
+    document.getElementById('conhecimentos').textContent = "Tenho conhecimento em Word, Excel,e  estou em nivel médio em Programação.";
+});
